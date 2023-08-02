8e065155a35601b60a406ffbae480fd9db1859b5
Joazen <johnsons544@gmail.com>
2023-08-02 12:30:01 +0800

    Server building

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
-//Learning the synchronous & asynchronous
-
 const fs = require("fs");
+const http = require("http");
+const url = require("url");
 
-//Blocking, synchronous way
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-console.log(textIn);
+///////////////////////////////////
+///////// Files. Learning the synchronous & asynchronous.
 
-const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
-fs.writeFileSync("./txt/output.txt", textOut);
+// //Blocking, synchronous way
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
 
-console.log("file written");
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
 
-//Non-blocking, asynchronous way
-fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
-  console.log(data);
+// console.log("file written");
+
+// //Non-blocking, asynchronous way
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+//     fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+//       console.log(data3);
+
+//       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+//         console.log("Your file has benn written");
+//       });
+//     });
+//   });
+// });
+// console.log("Will read file!");
+
+///////////////////////////////////
+///////// Server
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  if (pathName === "/overview" || pathName === "/") {
+    res.end("Hello from the OVERVIEW!");
+  } else if (pathName === "/product") {
+    res.end("Hello from the PRODUCT!");
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("<p>Page not found!!</p>");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to requests on port 8000");
 });
